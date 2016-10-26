5f3c679efac8f1a0f2880558006a3a88b2abf82a
Sasha Karamazov <pochtovyy.sasha@gmail.com>
2016-10-26 12:07:24 +0300

    similar-products

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,57 @@
 (function ($) {$(document).ready(function () {
 
+    //carousel
+
+  $('.slider__controls-buttons').on('click', function(e) {
+    e.preventDefault();
+
+    var
+      $this = $(this),
+      container = $this.closest('.slider'),
+      list = container.find('.slider__list'),
+      items = container.find('.slider__item'),
+      activeSlide = items.filter('.active'),
+      nextSlide = activeSlide.next(),
+      prevSlide = activeSlide.prev(),
+      firstSlede = items.first(),
+      lastSlide = items.last(),
+      sliderOffset = container.offset().left,
+      reqPos = 0,
+      itemPosition = activeSlide.index(),
+      trigger = container.find('.triggers__item');
+
+    if ($(this).hasClass('slider__controls-buttons_next')) {
+      if (nextSlide.length) {
+        findReqPos(nextSlide);
+        removeActiveClass(nextSlide);
+      } else {
+        findReqPos(firstSlede);
+        removeActiveClass(firstSlede);
+      }
+    } else {
+      if (prevSlide.length) {
+        findReqPos(prevSlide);
+        removeActiveClass(prevSlide);
+      } else {
+        findReqPos(lastSlide);
+        removeActiveClass(lastSlide);
+      }
+    }
+    list.css('left', '-=' + reqPos + 'px');
+
+    function removeActiveClass (reqSlide) {
+      reqSlide.addClass('active').siblings().removeClass('active');
+      trigger.eq(itemPosition)
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+    }
+
+    function findReqPos (slide) {
+      reqPos = slide.offset().left - sliderOffset;
+    }
+  });
+
   	//slideshow
     
 	$('.slideshow__pic').on('click', function(e) {
